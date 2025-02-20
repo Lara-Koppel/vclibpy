87b185f0c5f8a26ceb34c3aabd4c746a2f1c78e5
Fabian Meyer-Gehlen <fabian.meyer-gehlen@rwth-aachen.de>
2025-02-20 11:49:21 +0100

    Add calc_states and iterate_p_3

--- vclibpy/flowsheets/standard_ejector_cycle.py
+++ vclibpy/flowsheets/standard_ejector_cycle.py
@@ -1,10 +1,113 @@
+import math
+import logging
 from abc import ABC
+
+import numpy as np
 
 from vclibpy.flowsheets.ejector_cycle import BaseEjectorCycle
 from vclibpy.media import ThermodynamicState
+from vclibpy import Inputs, FlowsheetState
+
+
+logger = logging.getLogger(__name__)
 
 
 class StandardEjectorCycle(BaseEjectorCycle, ABC):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, **kwargs):
+        """Initialize class with kwargs"""
+        self.max_err = kwargs.pop("max_err", 0.5)
+        self.min_iteration_step = kwargs.pop("min_iteration_step", 1)
+        self.show_iteration = kwargs.get("show_iteration", False)
+        self.use_quick_solver = kwargs.pop("use_quick_solver", True)
+        self.max_num_iterations = kwargs.pop("max_num_iterations", int(1e5))
+        self.step_max = kwargs.pop("step_max", 10000)
+        super().__init__(**kwargs)
+
+    def calc_states(self, p_1, p_2, inputs: Inputs, fs_state: FlowsheetState):
+        # Condenser outlet
+        self.set_condenser_outlet_based_on_subcooling(p_con=p_2, inputs=inputs)  #only for subcritical cycles
+        # Ejector
+        self.ejector.state_primary = self.condenser.state_outlet
+        # in the standard cycle the secondary inlet is the vapour from the evaporator outlet
+        self.ejector.state_secondary = self.med_prop.calc_state("PQ", p_1, 1)
+
+        p_3 = self.iterate_p3(p_1, p_2, inputs, fs_state)
+                
+        # Compressor
+        self.compressor.state_inlet = self.med_prop.calc_state("PQ", p_3, 1)
+        self.compressor.calc_state_outlet(p_outlet=p_2, inputs=inputs, fs_state=fs_state)
+
+        # Evaporator
+        _h_phase_seperator_liquid = self.med_prop.calc_state("PQ", self.ejector.state_outlet.p, 0).h
+        self.evaporator.state_inlet = self.med_prop.calc_state("PH", p_1, _h_phase_seperator_liquid)
+        self.evaporator.state_outlet = self.med_prop.calc_state("PQ", p_1, 1)
+        # Condenser inlet
+        self.condenser.state_inlet = self.compressor.state_outlet
+
+    def iterate_p3(self, p_1, p_2, inputs, fs_state):
+        # Settings
+        if self.use_quick_solver:
+            step_p3 = self.step_max
+        else:
+            step_p3 = self.min_iteration_step
+
+        p_3_history = []
+        error_m_flow_history = []
+        error_m_flow = np.nan
+        num_iterations = 0
+
+        # iterate p_ejector_outlet = p_3 to get matching m_flow from ejector and compressor
+        p_3 = (p_2 + p_1) / 2  #how to gues p_3?
+        while True:
+            if isinstance(self.max_num_iterations, (int, float)):
+                if num_iterations > self.max_num_iterations:
+                    logger.warning("Maximum number of iterations for p_3 %s exceeded. Stopping.",
+                                   self.max_num_iterations)
+                    return
+
+                if (num_iterations + 1) % (0.1 * self.max_num_iterations) == 0:
+                    logger.info("Info: %s percent of max_num_iterations for p_3 %s used",
+                                100 * (num_iterations + 1) / self.max_num_iterations, self.max_num_iterations)
+
+            p_3_history.append(p_3 / 1e5)
+            error_m_flow_history.append(error_m_flow)
+
+            # increase counter
+            num_iterations += 1
+
+            m_flow_ejector = self.ejector.calc_m_flow(p_3)
+            self.compressor.state_inlet = self.med_prop.calc_state("PQ", p_3, 1)
+            m_flow_compressor = self.compressor.calc_m_flow(inputs, fs_state)
+            error_m_flow = m_flow_ejector - m_flow_compressor
+
+            if error_m_flow < 0:
+                p_3 -= step_p3
+                continue
+            else:
+                if step_p3 > self.min_iteration_step:
+                    p_3 += step_p3
+                    step_p3 /= 10
+                    continue
+                elif error_m_flow > self.max_err:
+                    step_p3 = 1000
+                    p_3 += step_p3
+                    continue
+
+            # If still here, and the values are equal, we may break.
+            #if p_3 == p_3_next:
+                # Check if solution was too far away. If so, jump back
+                # And decrease the iteration step by factor 10.
+                #if step_p3 > self.min_iteration_step:
+                    #p_3_next = p_3 + step_p3
+                    #step_p3 /= 10
+                    #continue
+            logger.info("Breaking: p_3 Converged")
+            break
+            # Check if values are not converging at all:
+            #p_3_unique = set(p_3_history[-10:])
+            #if len(p_3_unique) == 2 and step_p3 == self.min_iteration_step:
+            #    logger.critical("Breaking: p_3 not converging at all")
+            #    break
+
+        return p_3
