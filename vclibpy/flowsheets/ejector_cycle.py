20ff36978aad55d8dee82183d3ca2f3336aaf72a
Fabian Meyer-Gehlen <fabian.meyer-gehlen@rwth-aachen.de>
2025-03-10 17:27:12 +0100

    Added init

--- vclibpy/flowsheets/ejector_cycle.py
+++ vclibpy/flowsheets/ejector_cycle.py
@@ -1,9 +1,12 @@
 import abc
 
-from vclibpy import Inputs, FlowsheetState
 from vclibpy.flowsheets import BaseCycle
+from vclibpy.components.expansion_valves.ejector import Ejector
+from vclibpy.components.compressors import Compressor
+from vclibpy.components.expansion_valves import ExpansionValve
 
-class BaseEjectorCycle(BaseCycle, abc.ABC)
+
+class BaseEjectorCycle(BaseCycle, abc.ABC):
     """
     Class for cycles using an ejector
     
@@ -12,3 +15,14 @@ class BaseEjectorCycle(BaseCycle, abc.ABC)
     See parent docstring for info on further assumptions and parameters.
     """
 
+    flowsheet_name = "Base Class of all ejector cycles - not for use of HP generation"
+
+    def __init__(self,
+                 ejector: Ejector,
+                 compressor: Compressor,
+                 expansion_valve: ExpansionValve,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.ejector = ejector
+        self.compressor = compressor
+        self.expansion_valve = expansion_valve
