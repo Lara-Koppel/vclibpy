a71be5d5720d9680b6e1410d4a0c9af01e06316e
Fabian Meyer-Gehlen <fabian.meyer-gehlen@rwth-aachen.de>
2025-03-23 11:22:29 +0100

    Added Phase Seperator and Metering Valve

--- vclibpy/flowsheets/standard_ejector_cycle.py
+++ vclibpy/flowsheets/standard_ejector_cycle.py
@@ -4,6 +4,9 @@ from abc import ABC
 
 import numpy as np
 
+from examples.e8_ejector import ejector
+from vclibpy.components.expansion_valves import ExpansionValve
+from vclibpy.components.phase_separator import PhaseSeparator
 from vclibpy.flowsheets.ejector_cycle import BaseEjectorCycle
 from vclibpy.media import ThermodynamicState
 from vclibpy import Inputs, FlowsheetState
@@ -14,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 class StandardEjectorCycle(BaseEjectorCycle, ABC):
 
-    def __init__(self, **kwargs):
+    def __init__(self,metering_valve: ExpansionValve, phase_seperator = PhaseSeparator, **kwargs):
         """Initialize class with kwargs"""
         self.max_err = kwargs.pop("max_err", 0.5)
         self.min_iteration_step = kwargs.pop("min_iteration_step", 1)
@@ -22,26 +25,37 @@ class StandardEjectorCycle(BaseEjectorCycle, ABC):
         self.use_quick_solver = kwargs.pop("use_quick_solver", True)
         self.max_num_iterations = kwargs.pop("max_num_iterations", int(1e5))
         self.step_max = kwargs.pop("step_max", 10000)
+        self.metering_valve = metering_valve
+        self.phase_seperator = phase_seperator
         super().__init__(**kwargs)
 
     def calc_states(self, p_1, p_2, inputs: Inputs, fs_state: FlowsheetState):
         # Condenser outlet
-        self.set_condenser_outlet_based_on_subcooling(p_con=p_2, inputs=inputs)  #only for subcritical cycles
+        self.set_condenser_outlet_based_on_subcooling(p_con=p_2, inputs=inputs)  # ToDO: Implement transcritical cycle
+
         # Ejector
         self.ejector.state_primary = self.condenser.state_outlet
         # in the standard cycle the secondary inlet is the vapour from the evaporator outlet
         self.ejector.state_secondary = self.med_prop.calc_state("PQ", p_1, 1)
 
         p_3 = self.iterate_p3(p_1, p_2, inputs, fs_state)
-                
+
+        # Phase Separator
+        self.phase_seperator.state_inlet = self.ejector.state_outlet
+
         # Compressor
-        self.compressor.state_inlet = self.med_prop.calc_state("PQ", p_3, 1)
+        self.compressor.state_inlet = self.phase_seperator.state_outlet_vapor
         self.compressor.calc_state_outlet(p_outlet=p_2, inputs=inputs, fs_state=fs_state)
+
+        # Metering Valve
+        self.metering_valve.state_inlet = self.phase_seperator.state_outlet_liquid
+        self.metering_valve.calc_outlet(p_outlet=p_1)
 
         # Evaporator
         _h_phase_seperator_liquid = self.med_prop.calc_state("PQ", self.ejector.state_outlet.p, 0).h
         self.evaporator.state_inlet = self.med_prop.calc_state("PH", p_1, _h_phase_seperator_liquid)
         self.evaporator.state_outlet = self.med_prop.calc_state("PQ", p_1, 1)
+
         # Condenser inlet
         self.condenser.state_inlet = self.compressor.state_outlet
 
