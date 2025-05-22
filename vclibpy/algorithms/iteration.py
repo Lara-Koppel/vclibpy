40d020281e0b56ee761947effc1e0551875b9a37
Fabian Meyer-Gehlen <fabian.meyer-gehlen@rwth-aachen.de>
2025-05-22 09:40:56 +0200

    Modified iteration to enable calculation of transcritical cycle

--- vclibpy/algorithms/iteration.py
+++ vclibpy/algorithms/iteration.py
@@ -40,10 +40,10 @@ class Iteration(Algorithm):
 
     def __init__(self, **kwargs):
         """Initialize class with kwargs"""
-        self.min_iteration_step = kwargs.pop("min_iteration_step", 1)
+        self.min_iteration_step = kwargs.pop("min_iteration_step", 10)
         self.show_iteration = kwargs.get("show_iteration", False)
         self.use_quick_solver = kwargs.pop("use_quick_solver", True)
-        self.min_allowed_dT_min = kwargs.pop("min_allowed_dT_min", 0.01)
+        self.max_err_dT_min = kwargs.pop("min_allowed_dT_min", 0.01)
         self.max_num_iterations = kwargs.pop("max_num_iterations", int(1e5))
         self.step_max = kwargs.pop("step_max", 10000)
         self.return_least_error_if_max_reached = kwargs.pop("return_least_error_if_max_reached", False)
@@ -75,8 +75,13 @@ class Iteration(Algorithm):
         error_eva_history = []
         dT_eva_history = []
         dT_con_history = []
-        error_con, dT_min_eva, dT_min_con, error_eva = np.nan, np.nan, np.nan, np.nan
+        p_1, p_2, error_con, dT_min_eva, dT_min_con, error_eva = np.nan, np.nan, np.nan, np.nan, np.nan, np.nan
         plot_last = -100
+        if flowsheet.flowsheet_name == "StandardTranscritical":
+            cop_history = []
+            current_cop = np.nan
+            differential_history = []
+            num_p_2_iterations = 1
 
         # First: Iterate with given conditions to get the 4 states and the mass flow rate:
         if self.show_iteration:
@@ -122,38 +127,41 @@ class Iteration(Algorithm):
                     logger.info("Info: %s percent of max_num_iterations %s used",
                                 100 * (num_iterations + 1) / self.max_num_iterations, self.max_num_iterations)
 
-            p_1 = p_1_next
-            p_2 = p_2_next
             p_1_history.append(p_1 / 1e5)
             p_2_history.append(p_2 / 1e5)
+            p_1 = p_1_next
+            p_2 = p_2_next
             error_con_history.append(error_con)
             error_eva_history.append(error_eva)
             dT_con_history.append(dT_min_con)
             dT_eva_history.append(dT_min_eva)
+            if flowsheet.flowsheet_name == "StandardTranscritical":
+                cop_history.append(current_cop)
 
             if self.show_iteration:
                 for ax in ax_iterations.flatten():
                     ax.clear()
                 iterations = list(range(len(p_1_history)))[plot_last:]
                 ax_iterations[0, 0].set_ylabel("error_eva in %")
-                ax_iterations[0, 1].set_ylabel("error_con in %")
+                ax_iterations[0, 1].set_ylabel("cop in -")
                 ax_iterations[1, 0].set_ylabel("$\Delta T_\mathrm{Min}$ in K")
                 ax_iterations[1, 1].set_ylabel("$\Delta T_\mathrm{Min}$ in K")
                 ax_iterations[2, 0].set_ylabel("$p_1$ in bar")
                 ax_iterations[2, 1].set_ylabel("$p_2$ in bar")
                 ax_iterations[0, 0].scatter(iterations, error_eva_history[plot_last:])
-                ax_iterations[0, 1].scatter(iterations, error_con_history[plot_last:])
+                ax_iterations[0, 1].scatter(iterations, cop_history[plot_last:])
                 ax_iterations[1, 0].scatter(iterations, dT_eva_history[plot_last:])
                 ax_iterations[1, 1].scatter(iterations, dT_con_history[plot_last:])
                 ax_iterations[2, 0].scatter(iterations, p_1_history[plot_last:])
                 ax_iterations[2, 1].scatter(iterations, p_2_history[plot_last:])
+                plt.pause(1e-3)
                 plt.draw()
-                plt.pause(1e-5)
+                #plt.pause(1e-4)
 
             # Increase counter
             num_iterations += 1
             # Check critical pressures:
-            if p_2 >= _p_max:
+            if flowsheet.flowsheet_name != "StandardTranscritical" and p_2 >= _p_max:
                 if step_p2 == self.min_iteration_step:
                     logger.error("Pressure too high. Inputs %s are infeasible.", inputs.get_name())
                     return
@@ -182,6 +190,10 @@ class Iteration(Algorithm):
                     raise err
                 return
 
+            Q_con = flowsheet.condenser.calc_Q_flow()
+            P_el = flowsheet.calc_electrical_power(fs_state=fs_state, inputs=inputs)
+            current_cop = Q_con / P_el
+
             if num_iterations == 1:
                 if self.save_path_plots is not None and self.show_iteration:
                     input_name = inputs.get_name()
@@ -192,35 +204,85 @@ class Iteration(Algorithm):
 
             if not isinstance(error_eva, float):
                 print(error_eva)
-            if error_eva < 0:
+
+            # print(f"Iteration {num_iterations}: p_1 = {p_1}, p_2 = {p_2}, error_eva = {error_eva}, error_con = {error_con}")
+
+            # Check if last pressure adjustment caused a change of sign in error
+            # If so, we went over the optimum and need to decrease the step size
+            if (np.sign(error_eva) != np.sign(error_eva_history[-1])
+                    and num_iterations > 1
+                    and not np.isclose(p_1, p_1_history[-2])):
+                step_p1 /= 10
+                # continue
+            if flowsheet.flowsheet_name != "StandardTranscritical" and (np.sign(error_con) != np.sign(error_con_history[-1])
+                    and num_iterations > 1
+                    and not np.isclose(p_2, p_2_history[-2]))\
+                    and not step_p2 <= 0.5 * step_p1:
+                step_p2 /= 10
+                # continue
+
+            # Check which heat exchanger has the bigger error. The corresponding pressure gets updated
+            # if abs(error_eva) > abs(error_con):
+            if error_eva < 0 and step_p1 > self.min_iteration_step:
                 p_1_next = p_1 - step_p1
+                num_p_2_iterations = 1
+                step_p2 = self.step_max
+                continue
+            elif error_eva < -1*self.max_err and dT_min_eva > self.max_err_dT_min:
+                step_p1 = 1000
+                p_1_next = p_1 - step_p1
+                num_p_2_iterations = 1
+                step_p2 = self.step_max
                 continue
             else:
                 if step_p1 > self.min_iteration_step:
                     p_1_next = p_1 + step_p1
-                    step_p1 /= 10
+                    num_p_2_iterations = 1
+                    step_p2 = self.step_max
                     continue
-                # Only increase if the error is bigger than zero (or the allowed error) and
-                # there is still a temperature difference left to change.
-                elif error_eva > self.max_err and dT_min_eva > self.min_allowed_dT_min:
+                elif error_eva > self.max_err and dT_min_eva > self.max_err_dT_min:
                     step_p1 = 1000
                     p_1_next = p_1 + step_p1
+                    num_p_2_iterations = 1
+                    step_p2 = self.step_max
                     continue
-
-            if error_con < 0:
-                p_2_next = p_2 + step_p2
-                continue
+            # else:
+            if flowsheet.flowsheet_name != "StandardTranscritical":
+                if error_con < 0:
+                    p_2_next = p_2 + step_p2
+                    continue
+                else:
+                    if step_p2 > self.min_iteration_step:
+                        p_2_next = p_2 - step_p2
+                        continue
+                    elif error_con > self.max_err and dT_min_con > self.max_err_dT_min:
+                        p_2_next = p_2 - step_p2
+                        step_p2 = 1000
+                        continue
             else:
-                if step_p2 > self.min_iteration_step:
+                if num_p_2_iterations == 1:
+                    # print(f"Iteration: 1, COP: {current_cop}")
                     p_2_next = p_2 - step_p2
-                    step_p2 /= 10
+                    num_p_2_iterations += 1
                     continue
-                # Only decrease if the error is bigger than zero (or the allowed error) and
-                # there is still a temperature difference left to change.
-                elif error_con > self.max_err and dT_min_con > self.min_allowed_dT_min:
-                    p_2_next = p_2 - step_p2
-                    step_p2 = 1000
-                    continue
+                else:
+                    # print(f"Iteration: {num_p_2_iterations}, COP: {current_cop}")
+                    local_differential = (current_cop - cop_history[-1]) / (p_2*1e-5 - p_2_history[-1])     #ToDO: COP calculation needs to take change of p_1 into account. Therefore error_eva should be iterated before this
+                    print(f"Iteration: {num_p_2_iterations}, current_cop: {current_cop}, last_cop: {cop_history[-1]}, current_p_2: {p_2*1e-5}, last_p_2: {p_2_history[-1]}, local_differential: {local_differential}")
+                    differential_history.append(local_differential)
+                    if num_p_2_iterations > 2 and np.sign(differential_history[-1]) != np.sign(differential_history[-2]):
+                        print("sign change in local_differential. Decreasing step_p2")
+                        step_p2 /= 10
+                    num_p_2_iterations += 1
+                    if step_p2 >= self.min_iteration_step:
+                        if local_differential > 0:
+                            # print("local_differential: ", local_differential, "increasing pressure")
+                            p_2_next = p_2 + step_p2
+                            continue
+                        else:
+                            # print("local_differential: ", local_differential, "decreasing pressure")
+                            p_2_next = p_2 - step_p2
+                            continue
 
             # If still here, and the values are equal, we may break.
             if p_1 == p_1_next and p_2 == p_2_next:
@@ -234,8 +296,8 @@ class Iteration(Algorithm):
                     p_1_next = p_1 + step_p1
                     step_p1 /= 10
                     continue
-                logger.info("Breaking: Converged")
-                break
+            logger.info("Breaking: Converged")
+            break
 
             # Check if values are not converging at all:
             p_1_unique = set(p_1_history[-10:])
@@ -255,6 +317,7 @@ class Iteration(Algorithm):
                     "error_eva": error_eva_history,
                     "dT_con": dT_con_history,
                     "dT_eva": dT_eva_history,
+                    "cop": cop_history,
                 }).to_excel(self.save_path_plots.joinpath(f"{inputs.get_name()}.xlsx"))
 
         flowsheet.iteration_converged = True
